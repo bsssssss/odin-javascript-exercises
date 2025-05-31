4290c02c952e447266cc7549f384a637bee4cb7e
Bss <mx.bss@proton.me>
2025-05-31 23:40:59 +0200

    done

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+const sumAll = function(...nums) {
 
+  for (let i of nums) {
+    if (typeof i !== "number" || i < 0 || !(Number.isInteger(i))) {
+      return "ERROR";
+    }
+  }
+
+  let sorted = nums.sort((a, b) => a - b);
+  let a = sorted[0];
+  let b = sorted[1];
+  let res = 0;
+
+  for (let i = a; i <= b; i++) {
+    res += i;
+  }
+  return res;
 };
 
 // Do not edit below this line
