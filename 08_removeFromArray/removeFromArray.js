52d62480a0513c6600200c19d4e9e404f66bcec7
Bss <mx.bss@proton.me>
2025-05-31 23:23:51 +0200

    done

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...itemToRemove) {
+  const result = [];
+
+  for (let i = 0; i < array.length; i++) {
+    if (!itemToRemove.includes(array[i])) {
+      result.push(array[i]);
+    }
+  }
+  return result;
 };
+
+removeFromArray([1, 2, 3], 3);
 
 // Do not edit below this line
 module.exports = removeFromArray;
