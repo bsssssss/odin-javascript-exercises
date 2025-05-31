51d6a21068baa46486ccb64c588873cfc5e4c866
Bss <mx.bss@proton.me>
2025-06-01 01:50:44 +0200

    ex11 done

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(v) {
+  let result = (v - 32) * (5 / 9)
+  return Number(result.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(v) {
+  let result = (v * (9 / 5) + 32);
+  return Number(result.toFixed(1));
 };
 
 // Do not edit below this line
