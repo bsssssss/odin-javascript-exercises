455aa72977753fbf57f8b8691aacc4b12db1cd4b
Bss <mx.bss@proton.me>
2025-06-07 04:21:17 +0200

    done

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,25 @@
-const palindromes = function () {
+// For each characters in the string
+//    is it alphanumeric char ?
+//    if not do nothing
+//    if yes push the char
 
+function isAlphanumeric(char) {
+  const lower = char.toLowerCase();
+  const code = lower.charCodeAt(0);
+  if ((code >= 48 && code <= 57) || (code >= 97 && code <= 122)) {
+    return true;
+  }
+}
+
+const palindromes = function (string) {
+  let arr = Array.from(string.toLowerCase());
+  arr = arr.filter((c) => isAlphanumeric(c));
+
+  if (arr.toString() === arr.reverse().toString()) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 // Do not edit below this line
