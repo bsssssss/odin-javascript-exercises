0bfb8c933b88d3013dc26b05f57f12263abbb55c
Bss <mx.bss@proton.me>
2025-06-08 03:49:00 +0200

    done

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (array) {
+    function calcAge(birth, death) {
+        if (death == undefined) {
+            death = new Date().getFullYear();
+        }
+        return death - birth;
+    }
+    return array.reduce((prev, next) => {
+        const prevAge = calcAge(prev.yearOfBirth, prev.yearOfDeath);
+        const nextAge = calcAge(next.yearOfBirth, next.yearOfDeath);
+        if (prevAge < nextAge) return next;
+        else return prev;
+    });
 };
 
 // Do not edit below this line
